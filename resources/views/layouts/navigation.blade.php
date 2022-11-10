bff1631eef03d606b297a22bfa418e6a252459a0
keigo-hiura <f7urjzery@gmail.com>
2022-11-10 16:03:34 +0900

    ナビゲーションバーの変更

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -15,7 +15,7 @@
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                        コーヒーみくじ
+                        ダッシュボード
                     </x-nav-link>
                 </div>
 
